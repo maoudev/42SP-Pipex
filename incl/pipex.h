0e7cb85fae2af45542bc60ba0583e364c2a60b8a
vcavalca <61808793+vcavalca@users.noreply.github.com>
2022-02-13 20:28:28 -0300

    Update

--- incl/pipex.h
+++ incl/pipex.h
@@ -6,15 +6,17 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 10:38:55 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/10/07 10:24:05 by vcavalca         ###   ########.fr       */
+/*   Updated: 2022/02/13 20:24:02 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
-#define PIPEX_H
+# define PIPEX_H
 
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+# include "libft/incl/libft.h"
 
 #endif
